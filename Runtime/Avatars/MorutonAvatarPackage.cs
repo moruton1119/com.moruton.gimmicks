f104359a9f088effc6cf4225f523ec841d7e972c
moruton1119 <moruton3@gmail.com>
2026-01-15 11:15:26 +0900

    fix: correct broken json structure in avatars asmdef to fix define symbols

--- Runtime/Avatars/MorutonAvatarPackage.cs
+++ Runtime/Avatars/MorutonAvatarPackage.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+#if MODULAR_AVATAR
+using nadena.dev.modular_avatar.core;
+#endif
 
 namespace Moruton.Gimmicks
 {
     // アバター用の基底クラス
+#if MODULAR_AVATAR
+    public abstract class MorutonAvatarPackage : AvatarTagComponent
+#else
     public abstract class MorutonAvatarPackage : MonoBehaviour
+#endif
     {
 
     }
